00b18d6b17f1509951d10dc454849bedc21e3b65
Geleia <luizcanf@gmail.com>
2023-11-07 15:38:58 -0300

    Exemplo de navegação no React Native

--- PpDM/App.js
+++ PpDM/App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function TelaInicial() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text>Página inicial</Text>
+      <Button title="Tela DOIS" onPress={() => navigation.navigate('Tela2')} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function Tela2() {
+  return (
+    <View style={styles.container}>
+      <Text>Página DOIS</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Home" onPress={() => navigation.navigate('Home')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Tela2" component={Tela2} />
+        <Stack.Screen name="Home" component={TelaInicial} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
