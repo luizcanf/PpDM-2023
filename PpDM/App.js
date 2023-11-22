fa932409b9b9b6b675a05e8422713231461a9d35
luizcanf <luizcanf@gmail.com>
2023-11-22 12:57:15 -0300

    Mudanças em sala (navegação)

--- PpDM/App.js
+++ PpDM/App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function TelaInicial() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste do React Native</Text>
+      <Button title="Cadastrar" onPress={()=>{
+          navigation.navigate('Cadastro')
+        }}/>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function TelaCadastro() {
+  return (
+    <View style={styles.container}>
+      <Text>Aqui vem o Cadastro do Cliente</Text>
+      <Button title="Voltar" onPress={()=>{
+          navigation.navigate('Início')
+        }}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Início" component={TelaInicial} />
+        <Stack.Screen name="Cadastro" component={TelaCadastro} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
